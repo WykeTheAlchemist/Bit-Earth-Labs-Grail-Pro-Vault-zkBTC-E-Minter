320de072dc24d71ff7392b8e4fb63cb917c51967
WykeTheAlchemist <168372115+WykeTheAlchemist@users.noreply.github.com>
2025-12-20 13:31:14 +0200

    Create install-oracle-service.sh

---
+++ bit-earth-poe/docs/install-oracle-service.sh
@@ -0,0 +1,10 @@
+# Clone repository
+git clone https://github.com/bit-earth-labs/oracle-node.git
+cd oracle-node
+
+# Configure
+cp .env.example .env
+# Edit .env with your settings
+
+# Start
+docker-compose up -d
